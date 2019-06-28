540284077a92bb0970f1c0c0e26f21e498ae35dc
m18201012585@163.com <m18201012585@163.com>
2019-06-28 21:34:33 +0800

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,7 +5,7 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
-    
+
     public function category()
     {
         return $this->belongsTo(Category::class);
@@ -14,5 +14,21 @@ class Topic extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->latest();
+                break;
+
+            default:
+                $query->latest('updated_at');
+                break;
+        }
+        // 预加载防止 N+1 问题
+        return $query->with('user', 'category');
     }
 }
