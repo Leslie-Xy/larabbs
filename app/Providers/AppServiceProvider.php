6b5421f1483e6775bcbad19d6d56470a7cce2b51
m18201012585@163.com <m18201012585@163.com>
2019-07-01 19:43:57 +0800

    用户发表评论

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\Topic;
+use App\Models\{Topic, Reply};
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Topic::observe(\App\Observers\TopicObserver::class);
+        Reply::observe(\App\Observers\ReplyObserver::class);
     }
 }
